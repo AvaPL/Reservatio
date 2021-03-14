63cb1532a5b7c65d144f9a9a02be5a26816fa6ae
AvaPL <241574@student.pwr.edu.pl>
2021-03-14 12:55:42 +0100

    [CI test] backend tests should fail.

--- backend/src/main/java/com/ziwg/reservatio/ReservatioApplication.java
+++ backend/src/main/java/com/ziwg/reservatio/ReservatioApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//public class ReservatioApplication {
+public class ReservatioApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReservatioApplication.class, args);
--- backend/src/test/java/com/ziwg/reservatio/ReservatioApplicationTests.java
+++ backend/src/test/java/com/ziwg/reservatio/ReservatioApplicationTests.java
@@ -1,5 +1,6 @@
 package com.ziwg.reservatio;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class ReservatioApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertEquals(true, false);
     }
 
 }
