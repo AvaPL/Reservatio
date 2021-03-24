775d6b752e7d6b9621026b099fcdeda0035c34ed
AvaPL <241574@student.pwr.edu.pl>
2021-03-24 23:16:47 +0100

    Created MinioUploader with its config.

---
+++ backend/src/main/java/com/ziwg/reservatio/images/MinioUploader.java
@@ -0,0 +1,50 @@
+package com.ziwg.reservatio.images;
+
+import io.minio.*;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import lombok.val;
+
+import java.io.File;
+
+@Slf4j
+@RequiredArgsConstructor
+public class MinioUploader {
+    private final MinioClient minioClient;
+    private final String minioBucket;
+
+    public ObjectWriteResponse upload(File file, String key) {
+        val path = file.getAbsolutePath();
+        return upload(path, key);
+    }
+
+    public ObjectWriteResponse upload(String filepath, String key) {
+        if (!bucketExists()) {
+            log.info("Bucket '" + minioBucket + "' does not exist");
+            makeBucket();
+        }
+        return uploadObject(filepath, key);
+    }
+
+    @SneakyThrows
+    private boolean bucketExists() {
+        BucketExistsArgs args = BucketExistsArgs.builder().bucket(minioBucket).build();
+        return minioClient.bucketExists(args);
+    }
+
+    @SneakyThrows
+    private void makeBucket() {
+        MakeBucketArgs args = MakeBucketArgs.builder().bucket(minioBucket).build();
+        minioClient.makeBucket(args);
+        log.info("Created new bucket '" + minioBucket + "'");
+    }
+
+    @SneakyThrows
+    private ObjectWriteResponse uploadObject(String filepath, String key) {
+        UploadObjectArgs args = UploadObjectArgs.builder().bucket(minioBucket).object(key).filename(filepath).build();
+        val response = minioClient.uploadObject(args);
+        log.info("Uploaded new object '" + key + "' to bucket '" + minioBucket + "'");
+        return response;
+    }
+}
---
+++ backend/src/main/java/com/ziwg/reservatio/images/MinioUploaderConfig.java
@@ -0,0 +1,27 @@
+package com.ziwg.reservatio.images;
+
+import io.minio.MinioClient;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.val;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "reservatio.minio")
+@Getter
+@Setter
+public class MinioUploaderConfig {
+
+    private String endpoint;
+    private String username;
+    private String password;
+    private String bucket;
+
+    @Bean
+    public MinioUploader minioUploader() {
+        val minioClient = MinioClient.builder().endpoint(endpoint).credentials(username, password).build();
+        return new MinioUploader(minioClient, bucket);
+    }
+}
