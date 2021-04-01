f823bfb6c05ad243bff900ba9c3f4498eaa896db
AvaPL <241574@student.pwr.edu.pl>
2021-04-01 20:20:48 +0200

    Employees and their details are now part of the state.

--- frontend/src/serviceprovider/employees/Employees.js
+++ frontend/src/serviceprovider/employees/Employees.js
@@ -4,10 +4,27 @@ import {Button, Col, Nav, Row, Tab} from "react-bootstrap";
 import './Employees.scss'
 
 class Employees extends Component {
+
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            employees: [
+                {
+                    name: "John Doe",
+                    services: ["Service A", "Service B", "Service C"]
+                },
+                {
+                    name: "Another John",
+                    services: ["Service B", "Service C"]
+                }
+            ]
+        }
+    }
+
     render() {
         return (
             <div className="employees-text m-4 m-lg-5">
-                <Tab.Container defaultActiveKey="first">
+                <Tab.Container defaultActiveKey={this.state.employees[0].name}>
                     <Row>
                         <Col className="align-self-center">
                             <h1 className="float-left employees-text font-weight-bold">Employee</h1>
@@ -21,33 +38,51 @@ class Employees extends Component {
                     </Row>
                     <Row className="employees-border py-3">
                         <Col className="font-weight-bold" sm={3}>
-                            <Nav variant="pills" className="flex-column">
-                                <Nav.Item>
-                                    <Nav.Link eventKey="first" className="employees-tab">Tab 1</Nav.Link>
-                                </Nav.Item>
-                                <Nav.Item>
-                                    <Nav.Link eventKey="second" className="employees-tab">Tab 2</Nav.Link>
-                                </Nav.Item>
-                            </Nav>
+                            {this.employeesNamesPanel()}
                         </Col>
                         <Col sm={9}>
-                            <Tab.Content>
-                                <Tab.Pane eventKey="first">
-                                    <span className="font-weight-bold" style={{fontSize: "150%"}}>Services:</span>
-                                    <ul>
-                                        <li>1</li>
-                                        <li>2</li>
-                                        <li>3</li>
-                                    </ul>
-                                </Tab.Pane>
-                                <Tab.Pane eventKey="second">
-                                    Test
-                                </Tab.Pane>
-                            </Tab.Content>
+                            {this.employeeServicesPanel()}
                         </Col>
                     </Row>
                 </Tab.Container>
             </div>
+        );
+    }
+
+    employeesNamesPanel() {
+        return (
+            <Nav variant="pills" className="flex-column">
+                {
+                    this.state.employees.map(employee =>
+                        <Nav.Item>
+                            <Nav.Link eventKey={employee.name} className="employees-tab">{employee.name}</Nav.Link>
+                        </Nav.Item>
+                    )
+                }
+            </Nav>
+        );
+    }
+
+    employeeServicesPanel() {
+        return (
+            <Tab.Content>
+                {
+                    this.state.employees.map(employee =>
+                        <Tab.Pane eventKey={employee.name}>
+                            <span className="font-weight-bold" style={{fontSize: "150%"}}>Services:</span>
+                            {
+                                <ul>
+                                    {
+                                        employee.services.map(service =>
+                                            <li>{service}</li>
+                                        )
+                                    }
+                                </ul>
+                            }
+                        </Tab.Pane>
+                    )
+                }
+            </Tab.Content>
         );
     }
 }
