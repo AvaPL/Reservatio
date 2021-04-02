30828f9c716238017f4ea1bd7890237fb71ba125
Student 241540 <241540@student.pwr.edu.pl>
2021-04-03 00:46:06 +0200

    Add Explore, Favourites and Search view

--- frontend/src/customer/explore/Explore.js
+++ frontend/src/customer/explore/Explore.js
@@ -1,10 +1,141 @@
 import React, {Component} from 'react';
+import "../../stylesheets/Customer.scss";
 
 class Explore extends Component {
+
+    state = {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        salonButtonClicked: false,
+        featuredSalons: [
+            {
+                name: "uBasi",
+                imageUrl: "https://pliki.propertydesign.pl/i/11/75/94/117594_r0_1140.jpg"
+            },
+            {
+                name: "Cool salon",
+                imageUrl: "https://welpmagazine.com/wp-content/uploads/2020/10/Filament-hair-salon1.jpg"
+            },
+            {
+                name: "Another salon",
+                imageUrl: "https://www.triss.com.pl/wp-content/uploads/granat-2768U-1.jpg"
+            }
+        ]
+    };
+
     render() {
         return (
-            <div>
-                <h1>Explore</h1>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <select className="Select-box">
+                            <option selected>Select service</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                    <div className="col">
+                        <select className="Select-box">
+                            <option selected>Select city</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-lg-4">
+                        <div className="card" style={{background: "#FDC2C6", marginTop: "5%"}}  >
+                            <img className="card-img-top" style={{aspectRatio: "16/9"}} src="https://pliki.propertydesign.pl/i/11/75/94/117594_r0_1140.jpg" alt="Card cap"/>
+                            <div className="card-img-overlay">
+                                <div className="row">
+                                    <div className="card"style={{width: "20%", background: "rgba(255,255,255,0.7)"}}>
+                                        <h2>5.0</h2>
+                                    </div>
+                                    <div className="card"style={{width: "60%", background: "transparent"}}>
+                                    </div>
+                                    <div className="card"style={{width: "20%", background: "transparent"}}>
+                                        <h2><i className="bi bi-star-fill"  style={{color: "white"}}> </i></h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="http://localhost:3000/not-found" className="btn-lg btn-#fc848b stretched-link" style={{textDecoration: "none", color: "#000000"}}>uBasi</a>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="card" style={{background: "#FDC2C6", marginTop: "5%"}}>
+                            <img className="card-img-top" style={{aspectRatio: "16/9"}} src="https://welpmagazine.com/wp-content/uploads/2020/10/Filament-hair-salon1.jpg" alt="Card cap"/>
+                            <div className="card-img-overlay">
+                                <div className="row">
+                                    <div className="card"style={{width: "20%", background: "rgba(255,255,255,0.7)"}}>
+                                        <h2>5.0</h2>
+                                    </div>
+                                    <div className="card"style={{width: "60%", background: "transparent"}}>
+                                    </div>
+                                    <div className="card"style={{width: "20%", background: "transparent"}}>
+                                        <h2><i className="bi bi-star-fill"  style={{color: "white"}}> </i></h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="http://localhost:3000/not-found" className="btn-lg btn-#fc848b stretched-link" style={{textDecoration: "none", color: "#000000"}}>Salon1</a>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="card" style={{background: "#FDC2C6", marginTop: "5%"}}>
+                            <img className="card-img-top" style={{aspectRatio: "16/9"}} src="https://pliki.propertydesign.pl/i/11/75/94/117594_r0_1140.jpg" alt="Card cap"/>
+                            <div className="card-img-overlay">
+                                <div className="row">
+                                    <div className="card"style={{width: "20%", background: "rgba(255,255,255,0.7)"}}>
+                                        <h2>5.0</h2>
+                                    </div>
+                                    <div className="card"style={{width: "60%", background: "transparent"}}>
+                                    </div>
+                                    <div className="card"style={{width: "20%", background: "transparent"}}>
+                                        <h2><i className="bi bi-star-fill"  style={{color: "white"}}> </i></h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="http://localhost:3000/not-found" className="btn-lg btn-#fc848b stretched-link" style={{textDecoration: "none", color: "#000000"}}>Salon2</a>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="card" style={{background: "#FDC2C6", marginTop: "5%"}}>
+                            <img className="card-img-top" style={{aspectRatio: "16/9"}} src="https://www.triss.com.pl/wp-content/uploads/granat-2768U-1.jpg" alt="Card cap" />
+                            <div className="card-img-overlay">
+                                <div className="row">
+                                    <div className="card"style={{width: "20%", background: "rgba(255,255,255,0.7)"}}>
+                                        <h2>5.0</h2>
+                                    </div>
+                                    <div className="card"style={{width: "60%", background: "transparent"}}>
+                                    </div>
+                                    <div className="card"style={{width: "20%", background: "transparent"}}>
+                                        <h2><i className="bi bi-star-fill"  style={{color: "white"}}> </i></h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="http://localhost:3000/not-found" className="btn-lg btn-#fc848b stretched-link" style={{textDecoration: "none", color: "#000000"}}>Salon3</a>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className="card" style={{background: "#FDC2C6", marginTop: "5%"}}>
+                            <img className="card-img-top" style={{aspectRatio: "16/9"}} src="https://pliki.propertydesign.pl/i/11/75/94/117594_r0_1140.jpg" alt="Card cap"/>
+                            <div className="card-img-overlay">
+                                <div className="row">
+                                    <div className="card"style={{width: "20%", background: "rgba(255,255,255,0.7)"}}>
+                                        <h2>5.0</h2>
+                                    </div>
+                                    <div className="card"style={{width: "60%", background: "transparent"}}>
+                                    </div>
+                                    <div className="card"style={{width: "20%", background: "transparent"}}>
+                                        <h2><i className="bi bi-star-fill"  style={{color: "white"}}> </i></h2>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="http://localhost:3000/not-found" className="btn-lg btn-#fc848b stretched-link" style={{textDecoration: "none", color: "#000000"}}>Salon4</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
