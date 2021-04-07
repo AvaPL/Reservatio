3026e30805777c23860ffdff04bb8bcf823b5c13
AvaPL <241574@student.pwr.edu.pl>
2021-04-07 22:36:11 +0200

    Fixed image uploads to Minio on docker when loading test data.

--- backend/src/test/java/com/ziwg/reservatio/minio/MinioUploaderMock.java
+++ backend/src/test/java/com/ziwg/reservatio/minio/MinioUploaderMock.java
@@ -1,4 +1,4 @@
-package com.ziwg.reservatio;
+package com.ziwg.reservatio.minio;
 
 import com.ziwg.reservatio.minio.MinioUploader;
 import io.minio.MinioClient;
@@ -6,11 +6,14 @@ import io.minio.ObjectWriteResponse;
 import okhttp3.Headers;
 
 import java.io.File;
+import java.io.InputStream;
 import java.nio.file.Path;
 
 public class MinioUploaderMock extends MinioUploader {
 
     private final String mockString = "mock";
+    private final ObjectWriteResponse mockResponse = new ObjectWriteResponse(Headers.of(),
+            mockString, mockString, mockString, mockString, mockString);
 
     public MinioUploaderMock(MinioClient minioClient, String minioBucket, String bucketPolicy) {
         super(minioClient, minioBucket, bucketPolicy);
@@ -23,12 +26,17 @@ public class MinioUploaderMock extends MinioUploader {
 
     @Override
     public ObjectWriteResponse upload(File file, String key) {
-        return new ObjectWriteResponse(Headers.of(), mockString, mockString, mockString, mockString, mockString);
+        return mockResponse;
     }
 
     @Override
     public ObjectWriteResponse upload(Path filepath, String key) {
-        return new ObjectWriteResponse(Headers.of(), mockString, mockString, mockString, mockString, mockString);
+        return mockResponse;
+    }
+
+    @Override
+    public ObjectWriteResponse upload(InputStream inputStream, String key, String contentType) {
+        return mockResponse;
     }
 
     @Override
