99be5bbe3623cb237c64cb53d7462e330c74a6ef
Adrian Urban <adrian.urban4@wp.pl>
2021-04-11 15:39:34 +0200

    Added CustomerReservationView

--- backend/src/main/java/com/ziwg/reservatio/repository/CustomerReservationViewRepository.java
+++ backend/src/main/java/com/ziwg/reservatio/repository/CustomerReservationViewRepository.java
@@ -2,9 +2,6 @@ package com.ziwg.reservatio.repository;
 
 import com.ziwg.reservatio.view.CustomerReservationView;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-@CrossOrigin(methods = RequestMethod.GET)
 public interface CustomerReservationViewRepository extends PagingAndSortingRepository<CustomerReservationView, Long> {
 }
