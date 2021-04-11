49a3be304788c4599175b095a48dd8a846a50b3d
Adrian Urban <adrian.urban4@wp.pl>
2021-04-11 17:46:29 +0200

    cors bypass

--- backend/src/main/java/com/ziwg/reservatio/repository/CustomerReservationViewRepository.java
+++ backend/src/main/java/com/ziwg/reservatio/repository/CustomerReservationViewRepository.java
@@ -2,6 +2,9 @@ package com.ziwg.reservatio.repository;
 
 import com.ziwg.reservatio.view.CustomerReservationView;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@CrossOrigin(methods = RequestMethod.GET)
 public interface CustomerReservationViewRepository extends PagingAndSortingRepository<CustomerReservationView, Long> {
 }
