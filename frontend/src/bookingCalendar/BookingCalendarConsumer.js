56355a34dbb3a95a7abd258636ef63721ed01976
Mateusz Gwiżdż <mateusz.gwizdz1@gmail.com>
2021-05-03 20:42:22 +0200

    minor

--- frontend/src/bookingCalendar/BookingCalendarConsumer.js
+++ frontend/src/bookingCalendar/BookingCalendarConsumer.js
@@ -8,11 +8,11 @@ import Calendar from 'react-calendar'
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import 'react-calendar/dist/Calendar.css';
 import cn from "classnames";
-import {authService} from "../../auth/AuthService";
-import {backendHost} from "../../Config";
+import {authService} from "../auth/AuthService";
+import {backendHost} from "../Config"
 
 export default function BookingCalendarConsumer() {
-    const { serviceproviderid, serviceid } = useParams();
+    const {serviceproviderid, serviceid} = useParams();
     const history = useHistory();
     const [selectedDate, setSelectedDate] = useState(new Date());
 
@@ -24,27 +24,27 @@ export default function BookingCalendarConsumer() {
         serviceName: "Strzyżenie",
         price: "60 zł",
         address: "ul. Owaka 4, Wrocław",
-        reservations: [
+        availableReservations: [
             {
                 id: 1,
                 time: "8:00",
                 date: new Date(2021, 3, 10),
                 consumerId: 1,
-                employee: 1,
+                employeeid: 1,
             },
             {
                 id: 2,
                 time: "8:45",
                 date: new Date(2021, 3, 10),
                 consumerId: null,
-                employee: 1,
+                employeeid: 1,
             },
             {
                 id: 3,
                 time: "9:30",
                 date: new Date(2021, 3, 10),
                 consumerId: null,
-                employee: 2,
+                employeeid: 2,
 
             },
             {
@@ -52,49 +52,49 @@ export default function BookingCalendarConsumer() {
                 time: "10:15",
                 date: new Date(2021, 3, 11),
                 consumerId: null,
-                employee: 2,
+                employeeid: 2,
             },
             {
                 id: 5,
                 time: "11:00",
                 date: new Date(2021, 3, 11),
                 consumerId: null,
-                employee: 2,
+                employeeid: 2,
             },
             {
                 id: 6,
                 time: "11:45",
                 date: new Date(2021, 3, 11),
                 consumerId: null,
-                employee: 1,
+                employeeid: 1,
             },
             {
                 id: 7,
                 time: "12:30",
                 date: new Date(2021, 3, 11),
                 consumerId: null,
-                employee: 1,
+                employeeid: 1,
             },
             {
                 id: 8,
                 time: "14:45",
                 date: new Date(2021, 3, 11),
                 consumerId: null,
-                employee: 2,
+                employeeid: 2,
             },
             {
                 id: 9,
                 time: "15:30",
                 date: new Date(2021, 3, 11),
                 consumerId: null,
-                employee: 1,
+                employeeid: 1,
             },
             {
                 id: 10,
                 time: "16:15",
                 date: new Date(2021, 3, 11),
                 consumerId: null,
-                employee: 2,
+                employeeid: 2,
             },
         ],
         employees: [
@@ -111,17 +111,37 @@ export default function BookingCalendarConsumer() {
         ],
     });
 
-    const [value,setValue]=useState('');
-    const handleSelect=(selectedEmployee)=>{
+    const [value, setValue] = useState('');
+    const handleSelect = (selectedEmployee) => {
         console.log(selectedEmployee);
         setValue(selectedEmployee)
+    }
+
+    const handleClick = (employeeid, serviceid) => {
+        authService.fetchAuthenticated(`${backendHost}/rest/addReservation/`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                serviceId: serviceid,
+                employeeId: employeeid,
+                customerId: authService.token?.entityId
+            })
+        }).then(response => {
+            if (!response.ok) {
+                throw new Error("Failed to post");
+            }
+            return response;
+        })
     }
 
     console.log("value: " + value)
     return (
         <>
             <div className={styles.mainImgWrapper}>
-                <img src={state.image} alt={state.name} className={styles.mainImg} />
+                <img src={state.image} alt={state.name} className={styles.mainImg}/>
                 {history.length > 0 && (
                     <div className={styles.mainBack}>
                         <Button
@@ -129,16 +149,16 @@ export default function BookingCalendarConsumer() {
                             onClick={() => history.goBack()}
                             className={styles.squareBox}
                         >
-                            <FontAwesomeIcon icon={faChevronLeft} />
+                            <FontAwesomeIcon icon={faChevronLeft}/>
                         </Button>
                     </div>
                 )}
             </div>
             <Container>
-                <div className={styles.calendarTable} >
+                <div className={styles.calendarTable}>
                     <div>
                         <label className={styles.rectangle}>
-                                {`${state.serviceName}
+                            {`${state.serviceName}
                         ${state.name} ${state.price}`}
                         </label>
                     </div>
@@ -148,20 +168,24 @@ export default function BookingCalendarConsumer() {
                                   className={cn(Calendar.css, styles.calendarStyle)}
                         />
                     </div>
-                        <DropdownButton className={styles.dropdownButton} title="Wybierz pracownika" onSelect={handleSelect} isExpanded={true}>
-                            {state.employees.map((employee) => (
-                                <Dropdown.Item eventKey={employee.id} >{`${employee.firstName} ${employee.lastName}`}</Dropdown.Item>
-                            ))}
-                        </DropdownButton>
+                    <DropdownButton className={styles.dropdownButton} title="Wybierz pracownika" onSelect={handleSelect}
+                                    isExpanded={true}>
+                        {state.employees.map((employee) => (
+                            <Dropdown.Item
+                                eventKey={employee.id}>{`${employee.firstName} ${employee.lastName}`}</Dropdown.Item>
+                        ))}
+                    </DropdownButton>
                 </div>
 
                 <Row className={styles.row}>
-                    {state.reservations.filter(reservation => reservation.date.getTime() === selectedDate.getTime() && reservation.available === 1 && reservation.employee == value).map((reservation) => (
-                        <Col sm={12} md={6} lg={4} className={cn(styles.marginBottom, styles.paddingTop)} key={reservation.id}>
+                    {state.availableReservations.filter(reservation => reservation.date.getTime() === selectedDate.getTime() && reservation.consumerId === null && reservation.employeeid == value).map((reservation) => (
+                        <Col sm={12} md={6} lg={4} className={cn(styles.marginBottom, styles.paddingTop)}
+                             key={reservation.id}>
                             <AvailableService
                                 {...reservation}
-                                serviceproviderid={serviceproviderid}
-                                serviceid={serviceid}
+                                time={reservation.time}
+                                employeeid={reservation.employeeid}
+                                serviceid={reservation.id}
                             />
                         </Col>
                     ))}
@@ -169,19 +193,16 @@ export default function BookingCalendarConsumer() {
             </Container>
         </>
     );
+
+    function AvailableService({time, employeeid, serviceid}) {
+        return (
+            <Button
+                className={cn(styles.button, styles.serviceCardPriceInfoCol)}
+                variant="primary"
+                onClick={() => handleClick(employeeid, serviceid)}
+            >
+                {time}
+            </Button>
+        );
+    }
 }
-
-function AvailableService({ time, serviceid, serviceproviderid }) {
-    return (
-                <Button
-                    className={cn(styles.button, styles.serviceCardPriceInfoCol)}
-                    variant="primary"
-                    onClick="handleClick()"
-                    // strzał do API (rezerwacja)
-                >
-                    {time}
-                </Button>
-    );
-}
-
-
